7635e84cca5e04ee34f8f125289e2ee1f00d0427
Luis Fernando Rubio <luford82@gmail.com>
2024-05-03 19:55:22 +0200

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import axios from 'axios';
 /* Importamos el hook useParams para poder acceder desde nuestro componente
-a los parámetros de la ruta */
-import { useParams } from 'react-router-dom';
+a los parámetros de la ruta y el hook useNavigate para realizar redirecciones
+entre URLs */
+import { useParams, useNavigate } from 'react-router-dom';
 
 // Función para mostrar la modal que recibe por props la función de Cerrar Modal
 function Modal({ cierraModal }) {
 
   // En una constante almacenamos el valor id del post, recuperado de la URL
   const {id} = useParams();
+
+  /* Creamos la constante navegar que dispara el hook useNavigate
+    para redigir al usuario. Lo utilizaremos para llevarlo a la
+    home después de borrar un post */
+  const navegar = useNavigate();
+  
   // Función que borra el post con esa id
   function handleDelete () {
-    axios.delete( 'http://localhost:3000/postsById?id=' + id )
+    axios.delete( 'https://blogbackend1-0nyy.onrender.com/postsById?id=' + id )
     .then( response => {
       if (response.data.status === 'failed' ) {
         // Si la petición falla mostramos mensaje de error
         setPost({ error: 'Error: No se ha podio eliminar el post' });
       } else {
-          // En caso contrario es que el borrado se ha realizado con éxito
+          // En caso contrario es que el borrado se ha realizado con éxito.
           // Entonces navegamos de nuevo hasta la página de inicio
-          window.location.href = 'http://localhost:5173/inicio';
+         navegar('/inicio');
         }
     });
   };
